e500748d753414fbaa654f383f7d53a60ec9c05d
leejk206 <ljk9121@naver.com>
2025-05-08 19:36:26 +0900

    Init

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void LoadEditMode()
+    {
+
+    }
+
+    public void LoadPlayMode()
+    {
+
+    }
+
+    public void LoadChart()
+    {
+
+    }
+
+    public void SaveChart()
+    {
+
+    }
+}
